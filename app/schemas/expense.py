f7f7e1e4952859989f38ff1183218d5148273b4f
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2023-11-28 10:22:20 +0500

    value check

--- app/schemas/expense.py
+++ app/schemas/expense.py
@@ -1,11 +1,11 @@
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from app.models.expense import *
 
 class NewExpense(BaseModel):
     type: str
     employeeid: int
-    value: float
+    value: float = Field(..., gt=0, lt=1000000000)
     moneyformid: int
     comment: str
     userid: int
@@ -18,7 +18,7 @@ class NewExpense(BaseModel):
 class UpdateExpense(BaseModel):
     type: str
     employeeid: int
-    value: float
+    value: float = Field(..., gt=0, lt=1000000000)
     moneyformid: int
     comment: str
     userid: int
--- app/schemas/income.py
+++ app/schemas/income.py
@@ -1,17 +1,17 @@
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from app.models.income import *
 
 class NewIncome(BaseModel):
     clientId: int
-    value: float
+    value: float = Field(..., gt=0, lt=1000000000)
     moneyFormId: int
     comment: str
 
 
 class UpdateIncome(BaseModel):
     clientId: int
-    value: float
+    value: float = Field(..., gt=0, lt=1000000000)
     moneyFormId: int
     comment: str
 
