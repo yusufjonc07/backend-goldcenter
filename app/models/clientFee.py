3b0b6609c0e875fb33205f9d8d5f85732b628fb7
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2023-12-20 11:57:40 +0500

    additional

--- app/models/clientFee.py
+++ app/models/clientFee.py
@@ -1,16 +1,16 @@
 from sqlalchemy import Column, Date, Integer, Numeric,  Boolean, ForeignKey, func
 from databases.main import Base
 from sqlalchemy.orm import relationship
-from app.models.floor import * 
+from app.models.floor import *
 from sqlalchemy.dialects.mysql import DOUBLE
+
 
 class ClientFee(Base):
     __tablename__ = "clientFee"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     clientId = Column(Integer, ForeignKey('client.id'))
-    value = Column(Numeric)
+    value = Column(Numeric, default=0)
     isConfirmed = Column(Boolean, default=False)
     createdAt = Column(Date, default=func.now())
 
     client = relationship('Client')
-
