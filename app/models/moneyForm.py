6ddfbe312f0309fc8187b0eb61853fa70ae1ce32
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2024-02-24 16:09:45 +0500

    change

--- app/models/moneyForm.py
+++ app/models/moneyForm.py
@@ -1,11 +1,9 @@
-from sqlalchemy import Column, Date, Integer, Numeric, DateTime, Time, String, Boolean, Text, ForeignKey, func, TIMESTAMP
+from sqlalchemy import Column, Integer, Numeric, String
 from databases.main import Base
-from sqlalchemy.orm import relationship, backref
 
 
 class Moneyform(Base):
     __tablename__ = "moneyForm"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String, default='')
-
-
+    name = Column(String, nullable=False, unique=True)
+    balance = Column(Numeric, default=0)
--- events.py
+++ events.py
@@ -11,17 +11,5 @@ events_router = APIRouter()
 
 
 @events_router.on_event("startup")
-@repeat_every(seconds=300, wait_first=True)
 async def auto_committer():
-
     manager.active_connections = []
-
-    try:
-        with open("./data.txt", "w") as f:
-            longText = ''.join(random.choices(string.ascii_lowercase, k=1000))
-            longTextArr = longText.split('a')
-            f.write(" \n".join(longTextArr))
-            f.close()
-
-    except Exception as e:
-        print(e.args)
